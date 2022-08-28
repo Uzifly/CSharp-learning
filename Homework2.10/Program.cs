4434b675681b7975b188ced90adb4a51452802c0
Evgeni U <uzifly@gmail.com>
2022-08-28 18:05:50 +0300

    Заархивированы старые директории в ОЛД, выполнены 2.10 2.13

---
+++ Homework2.10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework2._10;
+class Program
+{
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+    static int InputNumber()
+    {
+        System.Console.Write("Введите трехзначное число: ");
+        return int.Parse(System.Console.ReadLine());
+    }
+    static int ShowSecondDigit(int number)
+    {
+        return (number / 10) % 10;
+    }
+    static void Main(string[] args)
+    {
+        int number = InputNumber();
+        System.Console.WriteLine($"Число: {number} \nВторая цифра: {ShowSecondDigit(number)}");
+    }
+}
---
+++ Homework2.13/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Homework2._13;
+class Program
+{
+
+    // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+    // 645 -> 5
+    // 78 -> третьей цифры нет
+    // 32679 -> 6
+    static int GetNumber()
+    {
+        System.Console.Write("Введите число: ");
+        return int.Parse(System.Console.ReadLine());
+    }
+
+    static int GetNumOfDigit(int number)
+    {
+        int i = 0;
+        while (number != 0) 
+        {
+            number /= 10;
+            i++;
+        }
+        return i;
+    }
+
+    static int[] GetDigFromNumber(int number, int numLenght)
+    {
+        int[] arrDig = new int[numLenght];
+        for (int i = 0; i < numLenght; i++)
+        {
+            arrDig[i] = number % 10;
+            number /= 10;
+        }
+        return arrDig;
+    }
+
+    static void PrintArrayPos(int[] array, int pos)
+    {
+        try
+        {
+            System.Console.WriteLine(array[array.Length-pos]);
+        }
+        catch (System.IndexOutOfRangeException)
+        {
+            System.Console.WriteLine("Такого числа нет");
+        }
+        
+    }
+
+    static void Main(string[] args)
+    {
+        int number = GetNumber();
+        int i = GetNumOfDigit(number);
+        int[] digit = GetDigFromNumber(number, i);
+        PrintArrayPos(digit, 3);
+    }
+}
