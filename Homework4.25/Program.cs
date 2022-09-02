b6d8831823e837017ae2c3d32aca7bdd1fe968cd
Evgeni U <uzifly@gmail.com>
2022-09-02 15:57:47 +0300

    Выполнено 4 ДЗ

---
+++ Homework4.25/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Homework4._25;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите степень");
+        int b = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine($"{a} в степени {b} = {Math.Pow(a,b)}");
+    }
+}
---
+++ Homework4.27/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework4._27;
+class Program
+{
+    static int GetSumDigit(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        System.Console.WriteLine($"Сумма цифр в числе {number} = {GetSumDigit(number)}");
+    }
+}
